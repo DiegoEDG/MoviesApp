7663c46281e15810f311ee05239a575db7b6057a
Digudev <1.5.dg.diego@gmail.com>
2023-06-26 09:50:13 -0600

    SETUP: Screens created

---
+++ src/screens/DetailScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, SafeAreaView, View} from 'react-native';
+
+const DetailScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{color: 'white'}}>DETAIL</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default DetailScreen;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={{color: 'white'}}>HOME</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
