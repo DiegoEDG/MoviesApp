5e95787d4d5e9fd47499a9462dfe6d0949779002
Digudev <1.5.dg.diego@gmail.com>
2023-06-27 11:17:03 -0600

    UI: MoviePosterComponent added to HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {ActivityIndicator, SafeAreaView, StyleSheet, View} from 'react-native';
+import {MoviePoster} from '../components';
+import {useMoviesFetch} from '../hooks';
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView>
-      <View>
-        <Text style={{color: 'white'}}>HOME</Text>
-      </View>
-    </SafeAreaView>
-  );
+  const {moviesNowPlaying, isLoading} = useMoviesFetch();
+
+  if (isLoading) {
+    return (
+      <SafeAreaView>
+        <View style={styles.loader}>
+          <ActivityIndicator color="white" size={60} />
+        </View>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <SafeAreaView>
+        <MoviePoster imagePath={moviesNowPlaying[2]?.poster_path} />
+      </SafeAreaView>
+    );
+  }
 };
+export const styles = StyleSheet.create({
+  loader: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default HomeScreen;
