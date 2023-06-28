575ee9d81c446aa3d76d5e07262214ff7ec8e9e0
Digudev <1.5.dg.diego@gmail.com>
2023-06-28 11:04:46 -0600

    REFACTOR [useMoviesFetch]: Refactor to the hook and the interfaces and Axios api file

--- src/api/movies.ts
+++ src/api/movies.ts
@@ -4,7 +4,7 @@ const token =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMGI2ZTlmM2MxZjY0NmE4N2IzZWJmM2NlMDQ3M2MyOCIsInN1YiI6IjY0OTk5Y2RhYmJkMGIwMDE0NGY0ZDYzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kbe9q9byUzmuugo0vTeu3uVp2Lm307APllREuIA9KYU';
 
 const moviesApi = axios.create({
-  baseURL: 'https://api.themoviedb.org/3/movie/now_playing',
+  baseURL: 'https://api.themoviedb.org/3/movie',
   headers: {Authorization: `Bearer ${token}`},
 });
 
--- src/hooks/useMoviesFetch.ts
+++ src/hooks/useMoviesFetch.ts
@@ -1,21 +1,51 @@
-import {useEffect, useState} from 'react';
+import {useState, useEffect} from 'react';
 import moviesApi from '../api/movies';
-import {MoviesNowPlaying, Movies} from '../interfaces';
+import {MoviesApiResponse, Movie} from '../interfaces';
+
+interface MoviesState {
+  nowPlaying: Movie[];
+  popular: Movie[];
+  topRated: Movie[];
+  upcoming: Movie[];
+}
 
 const useMoviesFetch = () => {
-  const [moviesNowPlaying, setMoviesNowPlaying] = useState<Movies[]>([]);
+  const [movies, setMovies] = useState<MoviesState>({
+    nowPlaying: [],
+    popular: [],
+    topRated: [],
+    upcoming: [],
+  });
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    moviesApi.get<MoviesNowPlaying>('').then(resp => {
-      setMoviesNowPlaying(resp.data.results);
-    });
+  const fetchMovies = async () => {
+    const nowPlayingPromise = moviesApi.get<MoviesApiResponse>('/now_playing');
+    const popularPromise = moviesApi.get<MoviesApiResponse>('/popular');
+    const topRatedPromise = moviesApi.get<MoviesApiResponse>('/top_rated');
+    const UpcomingPromise = moviesApi.get<MoviesApiResponse>('/upcoming');
 
+    const response = await Promise.all([
+      nowPlayingPromise,
+      popularPromise,
+      topRatedPromise,
+      UpcomingPromise,
+    ]);
+
+    setMovies({
+      nowPlaying: response[0].data.results,
+      popular: response[1].data.results,
+      topRated: response[2].data.results,
+      upcoming: response[3].data.results,
+    });
     setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies();
   }, []);
 
   return {
-    moviesNowPlaying,
+    ...movies,
     isLoading,
   };
 };
